066b234e008334bc98582a5bffb46102837a9711
Tarik Tuzlakovic <tarik.tuzlakovic77@gmail.com>
2018-10-08 23:01:49 +0200

    Add vue.js application

--- single-spa.config.js
+++ single-spa.config.js
@@ -3,6 +3,8 @@ import {registerApplication, start} from 'single-spa';
 registerApplication('navBar', () => import ('./src/navBar/navBar.app.js').then(mod => mod.navBar), () => true);
 registerApplication('home', () => import('./src/home/home.app.js'), () => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
 registerApplication('angularJS', () => import ('./src/angularJS/angularJS.app.js'), pathPrefix('/angularJS'));
+registerApplication('vueJS', () => import ('./src/vue/vue.app.js'), pathPrefix('/vue'));
+
 
 start();
 
