a51710a204f2fa136e3d237b22da582928a54e41
almir.dodigovic <almir.dodigovic@gmail.com>
2018-10-23 21:16:57 +0200

    Remove not needed files. #643

--- portal/webpack.config.js
+++ portal/webpack.config.js
@@ -54,25 +54,9 @@ module.exports = {
         },
 		// Proxy config for development purposes. In production, you would configure you webserver to do something similar.
         proxy: {
-            "/app1": {
-                target: "http://localhost:9001",
-                pathRewrite: {"^/app1" : ""}
-            },
-            "/app2": {
-                target: "http://localhost:9002",
-                pathRewrite: {"^/app2" : ""}
-            },
-            "/app3": {
-                target: "http://localhost:9003",
-                pathRewrite: {"^/app3" : ""}
-            },
             "/app4": {
                 target: "http://localhost:9004",
                 pathRewrite: {"^/app4" : ""}
-            },
-            "/app5": {
-                target: "http://localhost:9005",
-                pathRewrite: {"^/app5" : ""}
             }
         }
     }
